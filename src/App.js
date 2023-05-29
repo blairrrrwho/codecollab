f09ab3027063b050dcde5b148e08f703f426ea3e
AaronDreyer <aaronottaway@gmail.com>
2023-05-29 19:51:03 -0400

    Footer
    Starting code for footer to practice my skills with tailwind. Footer is fixed to bottom of screen.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Nav from './components/Nav'
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
     <Nav />
     <div className='bg-yellow-800 w-full h-screen'></div>
+    <Footer></Footer>
     </>
   );
 }
