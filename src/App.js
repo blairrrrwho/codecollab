93d717fef54033486d3505c1e0896fa693128824
AaronDreyer <aaronottaway@gmail.com>
2023-05-29 21:49:54 -0400

    About.jsx
    Im attempting to get the navigation to properly switch between pages. Once this is done I will add the About page.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import './App.css';
-import Nav from './components/Nav'
+import React, { useState, useEffect } from 'react';
+import Nav from './components/Nav';
+import About from './components/About';
 import Footer from './components/Footer';
 import Postform from './components/Postform';
 
 
 function App() {
+  const [currentTab, handleTabChange] = useState('About');
+
+  useEffect(() => {
+    document.title = `Code Collab | ${currentTab}`;
+  }, [currentTab]);
+
+  const renderTab = () => {
+    if (currentTab === "About") {
+      return <About />;
+    } else if (currentTab === "Postform")
+    return <Postform />
+  };
+
   return (
     <>
-    <Nav />
-
+    <Nav currentTab={currentTab}
+    handleTabChange={handleTabChange} />
+    <main>{renderTab()}</main>
     <div className='bg-yellow-800 w-full h-screen'>
       <Postform />
     </div>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,48 +1,80 @@
-import React, { useState } from 'react'
-import Button from './Button'
+import React, { useState, useEffect } from 'react';
 
-const Nav = () => {
-    let Links =[
-        {name: "HOME", link:"/"},
-        {name: "ABOUT", link:"/"}
-    ];
-    let [open,setOpen]=useState(false);
-    return (
-        <div className='shadow-md w-full fixed top-0 left-0'>
-            <div className='md:flex items-center justify-between bg-white py-4 md:px-10 px-7'>
-            <div className='font-bold text-2xl cursor-pointer flex items-center font-[Monospace]
-            text-gray-800'>
-                <span className='text-3xl text-yellow-500 mr-1 pt-2'>
-                <ion-icon name="code-outline"></ion-icon>
-                </span>
-                CodeCollab
-            </div>
+const Nav = (props) => {
+  let [open, setOpen] = useState(false);
 
-            <div onClick={()=>setOpen(!open)} className='text-3xl absolute right-8 top-6 cursor-pointer md:hidden
-            '>
-            <ion-icon name={open ? 'close':'menu'}></ion-icon>   
-            </div>
+  const { currentTab, handleTabChange } = props;
 
-            <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static
-            bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500
-            ease-in ${open ? 'top-20 opacity-100':'top-[-490px]'} md:opacity-100 opacity-0`}>
-                {
-                    Links.map((Link)=>(
-                        <li key={Link.name} className='md:ml-8 text-xl md:my-0 my-7'>
-                            <a href={Link.link} className='text-gray-800 hover:text-yellow-500'>{Link.name}</a>
-                        </li>
-                    ))    
-                }
-                <Button>
-                    Login
-                </Button>
-                <Button>
-                    Sign Up
-                </Button>
-            </ul>   
-            </div>
+  useEffect(() => {
+    document.title = currentTab;
+  }, [currentTab]);
+
+  return (
+    <div className='shadow-md w-full fixed top-0 left-0'>
+      <div className='md:flex items-center justify-between bg-white py-4 md:px-10 px-7'>
+        <div className='font-bold text-2xl cursor-pointer flex items-center font-[Monospace] text-gray-800'>
+          <span className='text-3xl text-yellow-500 mr-1 pt-2'>
+            <ion-icon name="code-outline"></ion-icon>
+          </span>
+          CodeCollab
         </div>
-    )
-}
 
-export default Nav
+        <div
+          onClick={() => setOpen(!open)}
+          className='text-3xl absolute right-8 top-6 cursor-pointer md:hidden'
+        >
+          <ion-icon name={open ? 'close' : 'menu'}></ion-icon>
+        </div>
+
+        <ul
+          className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
+            open ? 'top-20 opacity-100' : 'top-[-490px]'
+          } md:opacity-100 opacity-0`}
+        >
+          <li>
+            <button
+              className={`bg-yellow-700 text-white font-[Monospace] py-2 px-6 rounded md:ml-8 hover:bg-yellow-500 duration-500 mr-8 md:ml-8 md:mr-0 ${
+                currentTab === 'Home' ? 'bg-yellow-500' : ''
+              }`}
+              onClick={() => handleTabChange('Home')}
+            >
+              Home
+            </button>
+          </li>
+          <li>
+            <button
+              className={`bg-yellow-700 text-white font-[Monospace] py-2 px-6 rounded md:ml-8 hover:bg-yellow-500 duration-500 mr-8 md:ml-8 md:mr-0 ${
+                currentTab === 'About' ? 'bg-yellow-500' : ''
+              }`}
+              onClick={() => handleTabChange('About')}
+            >
+              About
+            </button>
+          </li>
+          <li>
+            <button
+              className={`bg-yellow-700 text-white font-[Monospace] py-2 px-6 rounded md:ml-8 hover:bg-yellow-500 duration-500 mr-8 md:ml-8 md:mr-0 ${
+                currentTab === 'Login' ? 'bg-yellow-500' : ''
+              }`}
+              onClick={() => handleTabChange('Login')}
+            >
+              Login
+            </button>
+          </li>
+          <li>
+            <button
+              className={`bg-yellow-700 text-white font-[Monospace] py-2 px-6 rounded md:ml-8 hover:bg-yellow-500 duration-500 mr-8 md:ml-8 md:mr-0 ${
+                currentTab === 'Sign Up' ? 'bg-yellow-500' : ''
+              }`}
+              onClick={() => handleTabChange('Sign Up')}
+            >
+              Sign Up
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
