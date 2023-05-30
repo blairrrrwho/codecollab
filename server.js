8e62a33b5dec9c38a59c7d2352b998d49004ac3f
Blair Millet <120157336+blairrrrwho@users.noreply.github.com>
2023-05-29 19:54:37 -0500

    added server.js; added license back; created routes folder

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+const cwd = process.cwd();
+
+const PORT = process.env.port || 3001;
+const app = express();
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
