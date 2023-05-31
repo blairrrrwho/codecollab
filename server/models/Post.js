bf5fe35c50c9463af3f327990d118237caea84c0
Blair Millet <120157336+blairrrrwho@users.noreply.github.com>
2023-05-30 19:50:28 -0500

    added post.js code for posts and comments

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,19 +1,85 @@
-const Comment = require("./Comment");
+// const Comment = require("./Comment");
 
-const {Schema, model} = require('mongoose');
+// const {Schema, model} = require('mongoose');
 
-const PostSchema = new Schema({
-    postText: {
-        type: String, 
-        required: true,
-        min: 1,
-        max: 250,
+// const PostSchema = new Schema({
+//     postText: {
+//         type: String, 
+//         required: true,
+//         min: 1,
+//         max: 250,
+//     },
+//     username: {
+//         type: String,
+//         required: true
+
+//     },
+//     comments: [Comment.schema] //indicates that any comments will be subdocuments 
+    
+// })
+
+const { Schema, model } = require('mongoose');
+
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxLength: 280,
     },
     username: {
-        type: String,
-        required: true
-
+      type: String,
+      required: true,
     },
-    comments: [Comment.schema] //indicates that any comments will be subdocuments 
-    
+    createdAt: {
+      type: Date,
+      default: Date.now
+    }
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false
+  }
+);
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true
+    },
+    id: false
+  }
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
 })
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
