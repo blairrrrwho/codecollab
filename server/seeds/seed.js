e4e822388a6f836c7cb192a53ac182060c8e0db5
William Kerby <w.kerby99@gmail.com>
2023-06-04 09:06:33 -0400

    add a seeds file, a seeds script in the package.json to seed data to db

---
+++ server/seeds/seed.js
@@ -0,0 +1,18 @@
+const db = require('../config/connection');
+const { User, Post } = require('../models');
+
+const userData = require('./userData.json');
+const postData = require('./postData.json');
+
+db.once('open', async () => {
+  // clean database
+  await User.deleteMany({});
+  await Post.deleteMany({});
+
+  // bulk create each model
+  const users = await User.insertMany(userData);
+  const posts = await Post.insertMany(postData);
+
+  console.log('data seed complete!');
+  process.exit(0);
+});
