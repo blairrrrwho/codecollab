2bdbb8ec499caeaaa2b6301433962211584e7f10
Carson Barnes <117684288+cbarnes0@users.noreply.github.com>
2023-06-05 19:25:59 -0400

    fixed expand function, rearranged/fixed postform

--- client/src/components/Postcard.jsx
+++ client/src/components/Postcard.jsx
@@ -3,74 +3,79 @@ import { useQuery } from '@apollo/client';
 import Avatar from 'boring-avatars';
 import { ALL_POSTS } from '../utils/queries';
 
-const Postcard = () => {
+const PostCard = ({ post }) => {
   const [expanded, setExpanded] = useState(false);
-  const [allPosts, setAllPosts] = useState([]);
-  
-  const { loading, data } = useQuery(ALL_POSTS, {
-    onCompleted: (data) => {
-      setAllPosts(data.allPosts);
-    },
-  });
-  
+
   const getReadingTime = (article) => {
     const wordsPerMinute = 250;
     const wordCount = article.split(' ').length;
     const readingTime = Math.ceil(wordCount / wordsPerMinute);
     return readingTime;
   };
-  
+
   const truncateText = (text, maxLength) => {
     if (text.length <= maxLength) {
       return text;
     }
     return text.slice(0, maxLength) + '...';
   };
-  
+
   const toggleExpand = () => {
     setExpanded(!expanded);
   };
-  
+
+  const truncatedArticle = truncateText(post.postText, 100);
+  const readingTime = getReadingTime(post.postText);
+
+  return (
+    <div key={post._id} className="bg-yellow-800 w-screen h-screen">
+      <div className="rounded-2xl relative p-6 bg-white mx-4 md:mx-auto md:max-w-lg lg:max-w-xl xl:max-w-2xl 2xl:max-w-3xl shadow-md text-left mt-32">
+        <div className="rounded-t-md h-16 w-full bg-yellow-100 absolute top-0 left-0 flex">
+          <h2 className="text-2xl text-gray-700 flex-1 mt-6 mx-4">React Resources</h2>
+          <div className="flex items-center mt-6 mx-4 text-gray-500 text-sm">
+            <span className="font-light">By @{post.username}</span>
+          </div>
+          <span className="w-16 h-16 mt-4 mr-8 z-10">
+            <Avatar
+              size={60}
+              name={post.username}
+              variant="beam"
+              colors={["#C1DDC7", "#F5E8C6", "#BBCD77", "#DC8051", "#F4D279"]}
+            />
+          </span>
+        </div>
+        <div className="mt-16">
+          <p>{expanded ? post.postText : truncatedArticle}</p>
+          <div className="flex flex-row justify-between">
+            <p className="mt-16 text-gray-400">{readingTime} minute read</p>
+            <p className="mt-16 text-extrabold underline cursor-pointer hover:text-gray-400" onClick={toggleExpand}>
+              {expanded ? 'X' : 'EXPAND'}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+// This is to have the expand feature be unique to each card without causing
+// issues with react hooks. 
+const Postcard = () => {
+  const [allPosts, setAllPosts] = useState([]);
+
+  const { loading, data } = useQuery(ALL_POSTS, {
+    onCompleted: (data) => {
+      setAllPosts(data.allPosts);
+    },
+  });
+
   return (
     <>
-      {allPosts.map((post) => {
-        const truncatedArticle = truncateText(post.postText, 100);
-        const readingTime = getReadingTime(post.postText);
-  
-        return (
-          <div key={post._id} className="bg-yellow-800 w-screen h-screen">
-            <div className="rounded-2xl relative p-6 bg-white mx-4 md:mx-auto md:max-w-lg lg:max-w-xl xl:max-w-2xl 2xl:max-w-3xl shadow-md text-left mt-32">
-              <div className="rounded-t-md h-16 w-full bg-yellow-100 absolute top-0 left-0 flex">
-                <h2 className="text-2xl text-gray-700 flex-1 mt-6 mx-4">React Resources</h2>
-                <div className="flex items-center mt-6 mx-4 text-gray-500 text-sm">
-                  <span className="font-light">By @{post.username}</span>
-                </div>
-                <span className="w-16 h-16 mt-4 mr-8 z-10">
-                  <Avatar
-                    size={60}
-                    name={post.username}
-                    variant="beam"
-                    colors={["#C1DDC7", "#F5E8C6", "#BBCD77", "#DC8051", "#F4D279"]}
-                  />
-                </span>
-              </div>
-              <div className="mt-16">
-                <p>{expanded ? post.postText : truncatedArticle}</p>
-                <div className="flex flex-row justify-between">
-                  <p className="mt-16 text-gray-400">{readingTime} minute read</p>
-                  <p className="mt-16 text-extrabold underline cursor-pointer hover:text-gray-400" onClick={toggleExpand}>
-                    {expanded ? 'X' : 'EXPAND'}
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        );
-      })}
+      {allPosts.map((post) => (
+        <PostCard key={post._id} post={post} />
+      ))}
     </>
   );
 };
-  
 
-  
 export default Postcard;
