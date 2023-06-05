18a81b3b8c79b6b6422cd72166d955c33d74b715
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-06-05 19:27:14 -0400

    fixed error in nav for to htmlFor

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -60,15 +60,15 @@ const Nav = () => {
                                         {/* <!-- Component Start --> */}
                                         <form className="flex flex-col bg-white rounded shadow-lg p-12 mt-12" action="" >
                                         <h1 className="font-bold text-2xl">Welcome! :)</h1>
-                                            <label className="font-semibold text-xs mt-3" for="usernameField">First Name</label>
+                                            <label className="font-semibold text-xs mt-3" htmlFor="usernameField">First Name</label>
                                             <input className="flex items-center h-12 px-4 w-64 bg-gray-200 mt-2 rounded focus:outline-none focus:ring-2" type="text" />
-                                            <label className="font-semibold text-xs mt-3" for="passwordField">Last Name</label>
+                                            <label className="font-semibold text-xs mt-3" htmlFor="passwordField">Last Name</label>
                                             <input className="flex items-center h-12 px-4 w-64 bg-gray-200 mt-2 rounded focus:outline-none focus:ring-2" type="text" />
-                                            <label className="font-semibold text-xs mt-3" for="passwordField">Username</label>
+                                            <label className="font-semibold text-xs mt-3" htmlFor="passwordField">Username</label>
                                             <input className="flex items-center h-12 px-4 w-64 bg-gray-200 mt-2 rounded focus:outline-none focus:ring-2" type="text" />
-                                            <label className="font-semibold text-xs mt-3" for="passwordField">Email</label>
+                                            <label className="font-semibold text-xs mt-3" htmlFor="passwordField">Email</label>
                                             <input className="flex items-center h-12 px-4 w-64 bg-gray-200 mt-2 rounded focus:outline-none focus:ring-2" type="text" />
-                                            <label className="font-semibold text-xs mt-3" for="passwordField">Password</label>
+                                            <label className="font-semibold text-xs mt-3" htmlFor="passwordField">Password</label>
                                             <input className="flex items-center h-12 px-4 w-64 bg-gray-200 mt-2 rounded focus:outline-none focus:ring-2" type="password" />
                                             <button className="flex items-center justify-center h-12 px-6 w-64 bg-blue-600 mt-8 rounded font-semibold text-sm text-blue-100 hover:bg-blue-700">Sign Up!</button>
                                         </form>
