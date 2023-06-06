400f0b4a18a9c90ea5eeb16891122090888ff7c9
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-06-05 20:56:35 -0400

    added to mutation.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,37 @@
 import { gql } from '@apollo/client';
 
+
+export const ADD_POST = gql`
+mutation Mutation($postText: String!, $username: String!, $postTitle: String!) {
+  addPost(postText: $postText, username: $username, postTitle: $postTitle) {
+    _id
+    postTitle
+    postText
+    username
+    createdAt
+    comments {
+      _id
+      commentBody
+    }
+  }
+}
+`
+export const ADD_USER = gql`
+mutation Mutation($firstname: String!, $lastname: String!, $username: String!, $email: String!, $password: String!) {
+  addUser(firstname: $firstname, lastname: $lastname, username: $username, email: $email, password: $password) {
+    token
+    user {
+      _id
+      email
+      firstname
+      lastname
+      password
+      username
+    }
+  }
+}
+`
+
 export const ADD_COMMENT = gql`
   mutation AddComment($postId: ID!, $commentBody: String!, $username: String!) {
     addComment(postId: $postId, commentBody: $commentBody, username: $username) {
