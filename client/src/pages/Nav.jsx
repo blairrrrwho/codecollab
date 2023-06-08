c431fff1680750e2ff3ad928b0f682845f0ba96b
Christina Hall <cmcclai26@gmail.com>
2023-06-08 19:39:34 -0400

    profile and log out buttons

--- client/src/pages/Nav.jsx
+++ client/src/pages/Nav.jsx
@@ -1,20 +1,10 @@
 import React, { useState } from 'react'
-import Button from './Button'
 
 const Nav = () => {
-    const [showModal, setShowModal] = useState(false);
-
-    const openModal = () => {
-        setShowModal(true);
-    };
-    const closeModal = () => {
-        setShowModal(false);
-    }
-
-    let Links = [
-        { name: "HOME", link: "/" },
-        { name: "ABOUT", link: "/" }
-    ];
+    // let Links = [
+    //     { name: "HOME", link: "/" },
+    //     { name: "ABOUT", link: "/" }
+    // ];
     let [open, setOpen] = useState(false);
     return (
         <div className='shadow-md w-full fixed top-0 left-0 z-20'>
@@ -35,51 +25,22 @@ const Nav = () => {
                 <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static
             bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500
             ease-in ${open ? 'top-20 opacity-100' : 'top-[-490px]'} md:opacity-100 opacity-0`}>
-                    {
+                    {/* {
                         Links.map((Link) => (
                             <li key={Link.name} className='md:ml-8 text-xl md:my-0 my-7'>
                                 <a href={Link.link} className='text-gray-800 hover:text-yellow-500'>{Link.name}</a>
                             </li>
                         ))
-                    }
-                    <Button>
-                        Login
-                    </Button>
-                    <button className='bg-yellow-700 text-white font-[Monospace] py-2 px-6 rounded md:ml-8 hover:bg-yellow-500
-                    duration-500 mr-8 md:ml-8 md:mr-0' onClick={openModal}>SignUp
+                    } */}
+                    {/* User profile icon */}
+                    <div className="flex flex-col items-center text-3xl m-1 mt-2 text-gray-800 hover:text-amber-500 hover:cursor-pointer">
+                    <ion-icon name="person-circle-outline"></ion-icon>
+                    <p className="text-gray-800 text-xs font-[Monospace]">Profile</p>
+                    </div>
+
+                    <button className='bg-rose-700 text-white font-[Monospace] py-2 px-6 rounded md:ml-8 hover:bg-amber-500
+                    duration-500 mr-8 md:ml-8 md:mr-0'>Logout
                     </button>
-                    {/* signup modal */}
-                    {showModal && (
-                        <div>
-                            <div className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center">
-                                <div className="w-[600px]">
-                                    {/* this is where the content stuff goes */}
-                                    <div className="flex flex-col items-center justify-center w-screen h-screen text-gray-700">
-                                    <button className="text-black text-xl place-self-end" onClick={closeModal}>X</button>
-
-                                        {/* <!-- Component Start --> */}
-                                        <form className="flex flex-col bg-white rounded shadow-lg p-12 mt-12" action="" >
-                                        <h1 className="font-bold text-2xl">Welcome! :)</h1>
-                                            <label className="font-semibold text-xs mt-3" htmlFor="usernameField">First Name</label>
-                                            <input className="flex items-center h-12 px-4 w-64 bg-gray-200 mt-2 rounded focus:outline-none focus:ring-2" type="text" />
-                                            <label className="font-semibold text-xs mt-3" htmlFor="passwordField">Last Name</label>
-                                            <input className="flex items-center h-12 px-4 w-64 bg-gray-200 mt-2 rounded focus:outline-none focus:ring-2" type="text" />
-                                            <label className="font-semibold text-xs mt-3" htmlFor="passwordField">Username</label>
-                                            <input className="flex items-center h-12 px-4 w-64 bg-gray-200 mt-2 rounded focus:outline-none focus:ring-2" type="text" />
-                                            <label className="font-semibold text-xs mt-3" htmlFor="passwordField">Email</label>
-                                            <input className="flex items-center h-12 px-4 w-64 bg-gray-200 mt-2 rounded focus:outline-none focus:ring-2" type="text" />
-                                            <label className="font-semibold text-xs mt-3" htmlFor="passwordField">Password</label>
-                                            <input className="flex items-center h-12 px-4 w-64 bg-gray-200 mt-2 rounded focus:outline-none focus:ring-2" type="password" />
-                                            <button className="flex items-center justify-center h-12 px-6 w-64 bg-blue-600 mt-8 rounded font-semibold text-sm text-blue-100 hover:bg-blue-700">Sign Up!</button>
-                                        </form>
-                                        {/* <!-- Component End  --> */}
-
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    )}
-                    <div />
                 </ul>
             </div>
         </div>
