ce8bee1228c3284331db140bebaa03dba36f1d9b
William Kerby <w.kerby99@gmail.com>
2023-06-08 21:52:28 -0400

    saved the path constant

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const db = require('./config/connection');
 // const routes = require('./routes');
-const path = require('path')
+const path = require('path');
 
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth')
